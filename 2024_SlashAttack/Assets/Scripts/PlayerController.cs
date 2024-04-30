96d515e02d46c483608a6693c326b033155caf57
ChainsFox <shykid4242@gmail.com>
2024-04-30 22:16:17 +0700

    hh

---
+++ 2024_SlashAttack/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Animator animator;
+    [Header("Horizontal Movement")]
+    Vector2 moveInput;
+    public float walkSpeed = 12f;
+    public float runSpeed = 18f;
+    public float airWalkSpeed = 18f;
+    public float jumpImpulse = 10f;
+
+    TouchingDirections touchingDiretions;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDiretions = GetComponent<TouchingDirections>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+    }
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                //flip the local scale to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1);
+
+            }
+            _isFacingRight = value;
+
+        }
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            //face right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            //face left
+            IsFacingRight = false;
+        }
+    }
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+                if (IsMoving && !touchingDiretions.IsOnWall)
+                {
+                    if (touchingDiretions.IsGrounded)
+                    {
+                        if (IsRunning)
+                        {
+                            return runSpeed;
+                        }
+                        else
+                        {
+                            return walkSpeed;
+                        }
+                    }
+                    else
+                    {//Air move
+                        return airWalkSpeed;
+                    }
+
+                }
+                else
+                {
+                    //idle speed = 0;
+                    return 0;
+                }
+
+
+
+
+
+        }
+    }
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+
+
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+
+
+
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+
+        }
+
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        SetFacingDirection(moveInput);
+        IsMoving = moveInput != Vector2.zero;
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //TODO: Check if alive as well
+        if (context.started && touchingDiretions.IsGrounded)/*&& CanMove */
+        {
+            animator.SetTrigger(AnimationStrings.jumpTrigger);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+
+    }
+
+
+
+}
