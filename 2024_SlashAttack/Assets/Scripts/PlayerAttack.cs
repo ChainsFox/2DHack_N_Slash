646d45955512f84c7edcf9c921197ee68117c049
ChainsFox <shykid4242@gmail.com>
2024-06-29 01:45:03 +0700

    28/06/2024-fixes

--- 2024_SlashAttack/Assets/Scripts/PlayerAttack.cs
+++ 2024_SlashAttack/Assets/Scripts/PlayerAttack.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class Attack : MonoBehaviour
+public class PlayerAttack : MonoBehaviour
 {
     Collider2D attackCollider;//can be remove part 16
     public int attackDamage = 10;
     public Vector2 knockback = Vector2.zero;
+    //public Vector2 flipxKnockback = Vector2.zero;
 
     private void Awake()
     {
         attackCollider = GetComponent<Collider2D>();//can be remove part 16
 
     }
+
 
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,12 +29,11 @@ public class Attack : MonoBehaviour
         {
             //this is for flipping the knocback depend on whether we are facing the left or right
             //if x > 0 then apply knock back as normal, if not(then x is < 0) we apply the same force but reverse 
-            Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
-
-
+            //Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
+        
 
             //hit the target
-            bool gotHit = damageable.Hit(attackDamage, deliveredKnockback);
+            bool gotHit = damageable.Hit(attackDamage, knockback);
 
             if (gotHit)
                 Debug.Log(collision.name + " hit for " + attackDamage);
