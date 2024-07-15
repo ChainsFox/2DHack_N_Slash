c7cc3c6bad0cd6c36868a9e99087ba2c4bddfa3f
ChainsFox <shykid4242@gmail.com>
2024-07-15 17:48:39 +0700

    bgmusic

---
+++ 2024_SlashAttack/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource introSource, loopSource;
+    public float addedTime=0.111f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introSource.Play();//this will play at the start
+        loopSource.PlayScheduled(AudioSettings.dspTime + introSource.clip.length + addedTime);//we tell the loop source to start playing, taking the current time, and adding the intro length to it
+    }
+
+}
